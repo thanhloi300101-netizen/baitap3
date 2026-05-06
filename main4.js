584414eea15883b08486a50021e56fc660943c9e
thanhloi300101-netizen <thanhloi.300101@gmail.com>
2026-05-06 15:10:00 +0900

    Add files via upload

---
+++ main4.js
@@ -0,0 +1,4 @@
+let firstNumber = Number(prompt("nhap so thu nhat :"));
+let secondNumber = Number(prompt("nhap so thu hai :"));
+let sum = firstNumber + secondNumber ;
+alert("tong 2 so la : " + sum) ;
